fe8c7d275832f6d6897df6b0b83a9840fafb78ac
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-09 22:47:20 +0300

    Initial commit

---
+++ data_types.py
@@ -0,0 +1,41 @@
+#printing
+
+print("Hello world")
+print("My name is Grace...")
+print(5689) #Interger
+print("5689") #String
+print(True)
+print("True")
+
+#Declaring Variables
+name="Grace"
+print("Hi,",name)
+uni="Taita Taveta uni..."
+print(uni)
+
+#Conver
+first_name="Grace"
+last_name="Gaciuki"
+print(first_name+" "+last_name)
+
+#Comments
+#This is a single lined comment
+"""
+This is line one
+It is a comment
+It doesnt affect the program
+"""
+
+#Escape Sequence
+print("""
+This is line one. 
+This is line two. 
+This is line three.""")
+
+print("This is line one.\n\tThis line is indented.")
+print("this isn\'t my dress")
+print("He said,\"You are beautiful\"")
+print("I love pilau\smokies")
+#When putting slash 4
+print("\\")
+print("1\\4")
