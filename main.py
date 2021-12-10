281a65eff4e7ec7356f6325963f15d49ebbc963f
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-10 18:16:28 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+name="mombasa"
+print("i will visit ",name,"next week")
+
+student_name1="gaciuki "
+student_name2="grace"
+print(student_name1+""+student_name2,"will be suspended the day after tommorrow")
+
+food_type1="chapo beans"
+food_type2="ugali beef"
+print("Day will eat",food_type1,"while Grace will have",food_type2,"during super time.")
+
+name1="Irene"
+name2="Omondi"
+print(name1+"",name2,"will visit Mombasa next week.")
+
+
+print("she used to tell me that dreams are valid and the majority of them come true if you work towards them .\nAnd so i will keep on trying now then and work towards achieving them for the sake of what she said.\ni will never give up on dreams unless am dead")
+
+print("4\\5")
+
+print("i will go for a walk at Mariwenyi.\n\tWhen the sun shines up above the sky brightly.")
