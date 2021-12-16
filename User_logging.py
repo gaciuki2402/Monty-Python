9716ac0d2284eb92eb6cfe77f21dc7e7539dd185
qinyanjuidavid <davidkinyanjui052@gmail.com>
2021-12-16 19:55:07 +0300

    Removed add_user feature

--- User_logging.py
+++ User_logging.py
@@ -7,21 +7,18 @@ class Authenticator(object):
     def __init__(self,users={}):
         self.users = users
 
-    def add_user(self):
-        self.username = input("Username:")
-        self.password = input("password:")
-        if self.username in self.users:
-            raise ValueError("username already exists!")
-        if len(self.password) < 6:
-            raise ValueError("Password must be more than 6 values!")
-        return True
-
     def login(self):
         self.attempt_username = input("Username2:")
         self.attempt_password = input("Password2:")
+        self.encodedAttemptPassword = base64.b64encode(self.attempt_password.encode())
+        self.decodedAttemptPassword = self.encodedAttemptPassword.decode('utf-8')
+        print(self.decodedAttemptPassword)
         isLoggedIn = False
         if self.attempt_username in self.users:
-            if self.users[self.attempt_username] == self.attempt_password:
+            self.encodedPassword = base64.b64encode(self.users[self.attempt_username].encode())
+            self.decodedPassword = self.encodedPassword.decode('utf-8')
+            print(self.decodedPassword)
+            if self.decodedPassword == self.decodedAttemptPassword:
                 print("Login Successful")
                 self.Shop()
                 isLoggedIn = True
@@ -40,22 +37,10 @@ class Authenticator(object):
         """)
 
     def Logic(self):
-        account = eval(input("Enter 1 to create account and 2 to login:"))
         isLoggedIn = False
         while not isLoggedIn:
-            if account == 1:
-                add_user = self.add_user()
-                if add_user == True:
-                    login = self.login()
-                    break
-            elif account == 2:
-                login = self.login()
-                break
-            else:
-                print("Invalid Choice")
-                break
-
-
+            login = self.login()
+            break
 if __name__ == "__main__":
     auth = Authenticator({
         "grace": "password1234",
