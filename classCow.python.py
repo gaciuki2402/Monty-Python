85a71cc777707124b3e1c09879f77c104b93f1e6
gaciuki2402 <delbrivigrace@gmail.com>
2021-12-30 07:47:46 +0300

    initial commit

--- classCow.python.py
+++ classCow.python.py
@@ -1,23 +1,22 @@
 class Animal(object):
-    def __int__(self,Nickname,Breed):
+    def __int__(self,Nickname,Breed,Suspecies):
         self.Nickname=Nickname
         self.Breed=Breed
+        self.Suspecies=Suspecies
 
     def Details(self):
-        print(f"My name is {self.Nickname} and my breed is {self.Breed}.")
+        print(f"My name is {self.Nickname}.My breed is {self.Breed} and {self.Suspecies} is my suspecies .")
 
-    def Suspecies(self,Suspecies):
-        print(f"{self.Nickname} is of {Suspecies} suspecies.")
 
 class Cow(Animal):
-    def __init__(self,Nickname,Breed):
-        super().__init__(Nickname,Breed)
+    def __init__(self,Nickname,Breed,Suspecies):
+        super().__init__(Nickname,Breed,Suspecies)
 
     def HornStatus(self,Horn):
-        print(f"{Suspecies} is naturally {Horn}.")
+        print(f"{self.Suspecies} is naturally {Horn}.")
 
     def Stayability(self,Stayability):
-        print(f"{Stayability} years old {Suspecies} are productive.")
+        print(f"{Stayability} years old {self.Suspecies} are productive.")
 
     def RegionOfOrigin(self,Origin):
         print(f"{self.Breed} originates from {Origin}.")
@@ -26,17 +25,16 @@ class Cow(Animal):
         print(f"{self.Breed} can either be {Coat}.")
 
     def Weight(self,Weight):
-        print(f"Female {Suspecies} has {Weight} kgs while Male {Suspecies} has {Weight}kgs.")
+        print(f"Female {self.Suspecies} has {Weight} kgs while Male {self.Suspecies} has {Weight}kgs.")
 
     def Height(self,Height):
-        print(f"Female {Suspecies} has a height of {Height} cm while Male {Suspecies} has a height of {Height} cm.")
+        print(f"Female {self.Suspecies} has a height of {Height} cm while Male {self.Suspecies} has a height of {Height} cm.")
 
     def GestationPeriod(self,Gestation):
-        print(f"{Suspecies} average gestation period is {Gestation} days. ")
+        print(f"{self.Suspecies} average gestation period is {Gestation} days. ")
 
-A1=Cow("Doddies","Aberdeen Angus")
+A1=Cow("Doddies","Aberdeen Angus","Angus")
 A1.Details()
-A1.Suspecies("Angus")
 A1.HornStatus("Polled horn")
 A1.Stayability("12 and 13")
 A1.RegionOfOrigin("Scotland")
